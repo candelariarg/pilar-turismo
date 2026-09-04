20a2ed395a02425a959d94f831cc034aec8043de
Mashuu <maurex05@gmail.com>
2026-09-03 22:33:00 -0300

    actualizacion index app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ThemedView } from '@/components/themed-view';
+import LugaresIniciales from '@/data/lugaresIniciales';
 
 export default function HomeScreen() {
   return (
     <ThemedView style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1, padding: 16 }}>
-        <Text>Inicio</Text>
+        <LugaresIniciales />
       </SafeAreaView>
     </ThemedView>
   );
