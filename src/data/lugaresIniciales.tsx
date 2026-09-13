eee2431045ed4ac906a763f779fbd3a8955e3e40
candelariarg <candyruggieri0@gmail.com>
2026-09-13 20:42:06 -0300

    arreglé problema con imagenes y subi iconos

--- src/data/lugaresIniciales.tsx
+++ src/data/lugaresIniciales.tsx
@@ -46,9 +46,9 @@ const LugaresIniciales = () => {
   const renderItem: ListRenderItem<Lugar> = ({ item }) => (
     <View style={styles.card}>
       <Image
-        source={{ uri: item.imagen_uri || require('../../assets/images/lugaresIniciales/parroquia.jpg') }} // Ruta de la imagen por defecto
-        style={styles.imagen}
-        resizeMode="cover"
+      source={ item.imagen_uri ? { uri: item.imagen_uri } : require('../../assets/images/lugaresIniciales/parroquia.jpg') }
+      style={styles.imagen}
+      resizeMode="cover"
       />
       <Text style={styles.titulo}>{item.Nombre}</Text>
       <Text style={styles.direccion}>Ubicación: {item.Dirección}</Text>
